f62d84b26bc52c8234778b641d22f9d1ba38e2b8
lyapustin <lyapustin@Dmitrys-iMac.local>
2018-05-03 17:46:52 +0300

    Added function “convert fills to images”; both functions now save existing layer name.

--- Convert Images to Fills.sketchplugin/Contents/Sketch/make-image.js
+++ Convert Images to Fills.sketchplugin/Contents/Sketch/make-image.js
@@ -1,3 +1,35 @@
+function convertFillsToImages(context) {
+
+  var document = context.document
+  var page = document.currentPage()
+  var selection = context.selection
+  var selectionCount = selection.length
+
+  if (selectionCount > 0) {
+    for (var i = 0; i < selectionCount; i++) {
+      var layer = selection.objectAtIndex(i)
+      var artboard = layer.parentArtboard()
+      var placeInto = (artboard) ? artboard : page
+
+      var image = layer.style().fills().firstObject().image();
+      var layerName = layer.name();
+      var layerFrame = layer.frame();
+      var layerImage = MSBitmapLayer.alloc().initWithFrame_image(
+        NSMakeRect(layerFrame.x(), layerFrame.y(), layer.rect().size.width, layer.rect().size.height),
+        image)
+
+      layerImage.frame().setWidth(layer.rect().size.width);
+      layerImage.frame().setHeight(layer.rect().size.height);
+      var addTo = (artboard) ? artboard : page;
+      addTo.addLayers([layerImage]);
+      layerImage.setName(layerName);
+      layerImage.setConstrainProportions(0);
+      layerImage.frame().size = layerImage.NSImage().size();
+      layer.removeFromParent()
+    }
+  }
+};
+
 function convertImagesToFills(context) {
 
   var document = context.document
@@ -24,7 +56,8 @@ function convertImagesToFills(context) {
 
         // create layer with image as fill
         var imageLayer = MSShapeGroup.shapeWithRect(layerRect)
-        imageLayer.setName("image")
+        var layerName = layer.name();
+        imageLayer.setName(layerName)
         imageLayer.setConstrainProportions(true)
         imageLayer.style().addStylePartOfType(0)
         var imageLayerStyle = imageLayer.style().fills().firstObject()
